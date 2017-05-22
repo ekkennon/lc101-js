c43fee7b36ee0ed330c976b54a09178ac28467d8
Erin K. Kennon <ekkennon@gmail.com>
2017-05-21 19:36:48 -0500

    finished studio 2

--- studios/mario/mario.js
+++ studios/mario/mario.js
@@ -1,28 +1,44 @@
 
-printPyramid(5);
+drawPyramid(5);
 
-/*
- * printPyramid
+
+/**
+ * drawPyramid
  *
- * Prints to the console a pyramid of '#' characters of the specified height
- * For example, if height is 5, the console will look like this:
- *          ##
- *         ###
- *        ####
- *       #####
- *      ######
+ * Renders, in the HTML document, a Mario pyramid of the specified height
  */
-function printPyramid(height) {
-	for (var i=0;i<height;i++) {
-		var row = "";
-		for (var j=0;j<height;j++) {
-			if (j < (height-1-i)) {
-				row += " ";
-			} else {
-				row += "#";
-			}
-		}
-		row += "#";
-		console.log(row);
-	}
+function drawPyramid(height) {
+
+    // TODO 2
+    // delete the "under construction" element, ie the <div id="notice">
+    var notice = document.getElementById("notice");
+    notice.parentNode.removeChild(notice);
+
+    // for each row....
+    for (var i = 0; i < height; i++) {
+
+        // figure out number of bricks and spaces
+        var numBricks = i + 2;
+        var numSpaces = height - i - 1;
+
+        // build up a string for this row
+        var rowStr = "";
+        for (var j = 0; j < numSpaces; j++) {
+            rowStr += "."; // QUIZ: what happens if we use a space (" ") instead of a period?
+        }
+        for (var j = 0; j < numBricks; j++) {
+            rowStr += "#";
+        }
+
+        // TODO 1
+        // create an element whose inner text is rowStr,
+        // and insert it as a child of the container <div id="pyramid">
+        var pyramid = document.getElementById("pyramid");
+        var paragraph = document.createElement("p");
+        var row = document.createTextNode(rowStr);
+        paragraph.appendChild(row);
+        pyramid.appendChild(paragraph);
+
+        
+    }
 }
